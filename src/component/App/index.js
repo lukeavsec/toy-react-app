30210ba89d09b6b1e388af7df7b782d19114b332
Luke Avsec <luke@avsec.io>
2022-10-03 08:21:03 -0400

    initial commit

---
+++ src/component/App/index.js
@@ -0,0 +1,16 @@
+import Table from '../Table'
+
+import './index.css';
+
+
+
+export default () => (
+	<div>
+		<h1>Toy React App</h1>
+		<p>
+			This app is built using Create React App and Ant Design.
+			It uses the Ant Design dark mode theme as well as the Ant Design table component.
+		</p>
+		<Table />
+	</div>
+)
